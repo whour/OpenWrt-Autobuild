602df19051e50008b3911fc482752ec646fe71e2
Register <458892+aieu@users.noreply.github.com>
2021-10-26 08:01:16 +0800

    bump to v21.02.1
    drop passwall
    remove V=s on the first build
    mv all defconfig into one folder
    replace libustream-openssl with libustream-wolfssl
    replace wpad with wpad-basic
    autocore: upstream
    build with default gcc version
    mbedtls: Camellia block cipher
    building ssr-libev with mbedtls
    ddns, serverchan, xlnetacc: drop on k2p and miwifi-mini
    drop ipv6helper & adbyby
    zeropi: Set dhcp protocol to get ip for lan
    mtk7615: co coolsnowwolf/lede
    try Caching toolchain
    
    Signed-off-by: Anonymous <458892+aieu@users.noreply.github.com>

--- scripts/k2p_customization.sh
+++ scripts/k2p_customization.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# drivers for mt7615
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/mt package/new/mt
-svn co https://github.com/Lienol/openwrt/trunk/package/lean/MTK7615-DBDC-LINUX5.4 package/new/mt
+# blob drivers for mt7615
+# Lean
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/mt package/new/mt
+sed -i 's|kmod-mt7615e kmod-mt7615-firmware|-luci-newapi -wpad-openssl kmod-mt7615d_dbdc wireless-tools|g' target/linux/ramips/image/mt7621.mk
+# Lienol
+#svn co https://github.com/Lienol/openwrt/trunk/package/lean/MTK7615-DBDC-LINUX5.4 package/new/mt
 wget -P feeds/luci/modules/luci-base/htdocs/luci-static/resources/icons/ https://github.com/coolsnowwolf/luci/raw/master/modules/luci-base/htdocs/luci-static/resources/icons/wifi_big.png
 wget -P feeds/luci/modules/luci-base/htdocs/luci-static/resources/icons/ https://github.com/coolsnowwolf/luci/raw/master/modules/luci-base/htdocs/luci-static/resources/icons/wifi_big_disabled.png
+
+# patch for mt76
+#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/kernel/mt76/patches package/kernel/mt76/patches
+
+# mbedtls
+cp -f ../patches/201-Camellia-block-cipher.patch package/libs/mbedtls/patches/
 
 exit 0
